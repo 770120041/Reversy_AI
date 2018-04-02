dcda5afbf12ba0b60d0285e881eaacded78389e4
Wu Jiang <wujiang5521@qq.com>
2018-04-02 10:19:14 +0800

    template

--- Reversi/readme.md
+++ Reversi/readme.md
@@ -1,1 +1,49 @@
 ## Reversi
+
+#### How to use
+
+Write your own engines using MCTS.
+
+Example:
+python reversi.py -a greedy -b random
+python reversi.py -a greedy -b random -t 30 -v
+python reversi.py -a human -b greedy -t 30
+
+
+
+#### Rule
+
+黑白棋（Othello/Reversi）
+
+游戏说明
+游戏规则：开局时，棋盘正中央的4格先置放黑白相隔的4枚棋子。通常黑子先行，双方轮流落子。只要落子和棋盘上任一枚己方的棋子在一条线上（横、直、斜线皆可）夹着对方棋子，就能将对方的这些棋子转变为己方（翻面即可）。如果在任一位置落子都不能夹住对手的任一颗棋子，就要让对手下子。当双方皆不能下子时，游戏就结束，子多的一方胜。
+时间限制：每个玩家下棋和思考花费的总时间不能超过30分钟。
+
+比赛说明
+比赛规则：
+1. 单循环赛（每个队与其他所有队伍都进行一场比赛，每两个队仅比赛一场，A与B比赛等同于B与A比赛）；
+2. 每场比赛胜利的队伍记1分，失败的队伍不计分，单循环赛结束后按总分进行排名；
+3. 每场比赛包括3局游戏，每局游戏胜利的队伍记该方棋子数目为该局得分，失败的队伍不计分，3局比赛后总分最高的该场比赛获胜，若双方总分相等则再加一局；若一方超时则判定本局游戏失败。
+
+重要时间
+组队报名时间：4月7日上午8点前
+代码提交时间：5月12日上午8点前
+报告提交时间：5月14日上午8点前
+
+组队说明
+组队人数：最多3人
+
+代码说明
+为保证公平性，使用Python语言，在同一台机器（配置：2 CPUs, 6 cores, 24 Intel(R) Xeon(R) CPU E5-2620 v3 @ 2.40GHz, 128G Mem, Ubuntu 14.04.5 LTS）上运行
+
+报告说明
+主要包含内容：
+1. 方法介绍及技巧
+2. 结果展示及分析
+3. 参考及引用
+
+-----
+参考链接：
+https://en.wikipedia.org/wiki/Reversi
+https://www.wikihow.com/Play-Othello
+http://www.othelloonline.org/
---
+++ Reversi/reversi.py
@@ -0,0 +1,174 @@
+from __future__ import print_function
+import argparse, copy, signal, sys, timeit, traceback
+from board import Board, move_string
+import random
+
+player = {-1 : "Black", 1 : "White"}
+
+def game(black_engine, white_engine, game_time=30.0, verbose=False):
+    """ Run a single game. Raise RuntimeError in the event of time expiration.
+    Raise LookupError in the case of a bad move. The tournament engine must
+    handle these exceptions. """
+
+    # Initialize variables
+    board = Board()
+    totaltime = { -1 : game_time*60, 1 : game_time*60 } # in seconds
+    engine = { -1 : black_engine, 1 : white_engine }
+
+    if verbose:
+        print("INITIAL BOARD\n\n--\n")
+        board.display(totaltime)
+
+    # Do rounds
+    for move_num in range(60):
+        moves = []
+        for color in [-1, 1]:
+            start_time = timeit.default_timer()
+            move = get_move(board, engine[color], color, move_num, totaltime)
+            end_time = timeit.default_timer()
+            # Update user totaltime
+            time = round(end_time - start_time, 1)
+            totaltime[color] -= time
+
+            if totaltime[color] < 0:
+                raise RuntimeError(color)
+
+            # Make a move, otherwise pass
+            if move is not None:
+                board.execute_move(move, color)
+                moves.append(move)
+
+                if verbose:
+                    print(("--\n\nRound {}: {} plays in {}\n"
+                        ).format(str(move_num + 1), player[color], move_string(move)))
+                    board.display(totaltime)
+
+        if not moves:
+            break # No more legal moves. Game is over.
+
+    print("\n--------------------\nFINAL BOARD\n--\n")
+    board.display(totaltime)
+
+    board.totaltime = totaltime
+    return board
+
+def get_move(board, engine, color, move_num, time, **kwargs):
+    """ Get the move for the given engine and color. Check validity of the
+    move. """
+    legal_moves = board.get_legal_moves(color)
+
+    if not legal_moves:
+        return None
+    elif len(legal_moves) == 1:
+        return legal_moves[0]
+    else:
+        try:
+            move = engine.get_move(copy.deepcopy(board), color, move_num, time[color], time[-color])
+        except Exception, e:
+            print(traceback.format_exc())
+            raise SystemError(color)
+
+        if move not in legal_moves:
+            print("legal list", [move_string(m) for m in legal_moves])
+            print("illegal", move_string(move), "=", move)
+            raise LookupError(color)
+
+        return move
+
+def winner(board):
+    """ Determine the winner of a given board. Return the points of the two
+    players. """
+    black_count = board.count(-1)
+    white_count = board.count(1)
+    if black_count > white_count:
+        return (-1, black_count, white_count)
+    elif white_count > black_count:
+        return (1, black_count, white_count)
+    else:
+        return (0, black_count, white_count)
+
+def signal_handler(signal, frame):
+    """ Capture SIGINT command. """
+    print('\n\n- You quit the game!')
+    sys.exit()
+
+def main(engines, user_names, scores, game_time, verbose):
+    try:
+
+        print(("\n====================\nNEW GAME\nBlack: {}\nWhite: {}").format(user_names[0], user_names[1]))
+        board = game(engines[0], engines[1], game_time, verbose)
+        stats = winner(board)
+        if stats[0] == -1: # black wins
+            scores[0]+=stats[1]
+            print(("- {} ({}) wins the game! (Current score: {})").format(user_names[0], player[-1], scores[0]))
+        elif stats[0] == 1: # white wins
+            scores[1]+=stats[2]
+            print(("- {} ({}) wins the game! (Current score: {})").format(user_names[1], player[1], scores[1]))
+        else:
+            print("- Tied!")
+
+    except RuntimeError, e:
+        err_usr_id = int((e[0]+1)/2) # 0 or 1
+        other_id = int((1-e[0])/2) # 1 or 0
+        scores[other_id]+=64
+        print(("\n- {} ({}) ran out of time!\n").format(user_names[err_usr_id], player[e[0]]))
+        print(("{} ({}) wins the game! (Current score: {})").format(user_names[other_id], player[e[0]*-1], scores[other_id]))
+    except LookupError, e:
+        err_usr_id = int((e[0]+1)/2) # 0 or 1
+        other_id = int((1-e[0])/2) # 1 or 0
+        scores[other_id]+=64
+        print(("\n- {} ({}) made an illegal move!\n").format(user_names[err_usr_id], player[e[0]]))
+        print(("{} ({}) wins the game! (Current score: {})").format(user_names[other_id], player[e[0]*-1], scores[other_id]))
+    except SystemError, e:
+        err_usr_id = int((e[0]+1)/2) # 0 or 1
+        other_id = int((1-e[0])/2) # 1 or 0
+        scores[other_id]+=64
+        print(("\n- {} ({}) ended prematurely because of an error!\n").format(user_names[err_usr_id], player[e[0]]))
+        print(("{} ({}) wins the game! (Current score: {})").format(user_names[other_id], player[e[0]*-1], scores[other_id]))
+    finally:
+        return scores
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
+    # Automatically generate help and usage messages.
+    # Issue errors when users gives the program invalid arguments.
+    parser = argparse.ArgumentParser(description="Play the Reversi/Othello game using different engines.")
+    parser.add_argument("-a", "--engine_a", action="store", type=str, default="greedy", help="first engine (human, eona, greedy, nonull, random)")
+    parser.add_argument("-b", "--engine_b", action="store", type=str, default="random", help="second engine (human, eona, greedy, nonull, random)")
+    parser.add_argument("-t", "--time", action="store", type=int, default=30, help="time limit (in minutes)")
+    parser.add_argument("-v", "--verbose", action="store_true", default=False, help="display the board at each turn")
+    args = parser.parse_args()
+
+    ename1 = args.engine_a # engine name 1
+    ename2 = args.engine_b # engine name 2
+    enames = [ename1, ename2]
+    print(("{} vs. {}").format(ename1, ename2))
+
+    try:
+        engines_1 = __import__('engines.' + ename1)
+        engines_2 = __import__('engines.' + ename2)
+        engine1 = engines_1.__dict__[ename1].__dict__['engine']()
+        engine2 = engines_2.__dict__[ename2].__dict__['engine']()
+        engines = [engine1, engine2]
+
+        v = (args.verbose or ename1 == "human" or ename2 == "human")
+
+        # Game begin
+        ss = [0]*2 # scores
+        n=0
+        while n<3 or ss[0]==ss[1]:
+            i,j = random.sample([0,1],2)
+            ss[i], ss[j] = main([engines[i], engines[j]], [enames[i], enames[j]], [ss[i], ss[j]], game_time=args.time, verbose=v)
+            n+=1
+
+        print(('\n========== FINAL REPORT ==========\n{} - {}: {} - {}'
+            ).format(enames[0], enames[1], ss[0], ss[1]))
+        if ss[0]>ss[1]:
+            print('The winner is', enames[0], '!')
+        else:
+            print('The winner is', enames[1], '!')
+
+    except ImportError, e:
+        print(('Unknown engine -- {}').format(e[0].split()[-1]))
+        sys.exit()
