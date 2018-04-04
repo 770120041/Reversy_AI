accc03d1bbd1dd66eb6f1df24afa1b484a690aba
Wu Jiang <wujiang5521@qq.com>
2018-04-04 13:47:41 +0800

    mcts core init

---
+++ Reversi/engines/MCTSCore/mcts_core.py
@@ -0,0 +1,11 @@
+class MctsCore:
+    def __init__(self, board, color):
+        self.board = board
+        self.color = color
+        pass
+
+    def cal(self):
+        pass
+
+    def current_best_move(self):
+        pass
--- Reversi/engines/mcts.py
+++ Reversi/engines/mcts.py
@@ -1,9 +1,32 @@
 from engines import Engine
+import Queue
+import multiprocessing
+from time import ctime, sleep
+from MCTSCore import mcts_core
 
 
 class MCTSEngine(Engine):
     def get_move(self, board, color, move_num=None, time_remaining=None, time_opponent=None):
-        moves = board.get_legal_moves(color)
+        bob = 21  # Whatever sensible value you need
+        queue = multiprocessing.Queue(1)  # Maximum size is 1
+        proc = multiprocessing.Process(target=self.wrapper, args=(queue, bob))
+        proc.start()
+
+        # Wait for TIMEOUT seconds
+        try:
+            result = queue.get(True, self.cal_time(self, move_num, time_remaining))
+        except Queue.Empty:
+            # Deal with lack of data somehow
+            result = None
+        finally:
+            proc.terminate()
+
+        # Process data here, not in try block above, otherwise your process keeps running
+        print result
+
+    @staticmethod
+    def cal_time(self, move_num, time_remaining):
+        return time_remaining / (60 - move_num)
 
 
 engine = MCTSEngine
