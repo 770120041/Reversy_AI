d7c27c830b2d461cb65890822780048350e41c8c
Wu Jiang <wujiang5521@qq.com>
2018-04-04 13:54:42 +0800

    code reformat

--- Reversi/board.py
+++ Reversi/board.py
@@ -16,16 +16,17 @@ x is the column, y is the row.
 """
 from __future__ import print_function
 
-class Board():
+
+class Board:
     # List of all 8 directions on the board, as (x,y) offsets
-    __directions = [(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1),(0,1)]
+    __directions = [(1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1)]
 
     def __init__(self):
         """ Set up initial board configuration. """
         # Create the empty board array
-        self.pieces = [None]*8
+        self.pieces = [None] * 8
         for i in range(8):
-            self.pieces[i] = [0]*8
+            self.pieces[i] = [0] * 8
 
         # Set up the initial 4 pieces
         self.pieces[3][4] = 1
@@ -41,9 +42,9 @@ class Board():
         """" Display the board and the statistics of the ongoing game. """
         print("    A B C D E F G H")
         print("    ---------------")
-        for y in range(7,-1,-1):
+        for y in range(7, -1, -1):
             # Print the row number
-            print(str(y+1) + ' |', end=' ')
+            print(str(y + 1) + ' |', end=' ')
             for x in range(8):
                 # Get the piece to print
                 piece = self[x][y]
@@ -53,14 +54,16 @@ class Board():
                     print("W", end=' ')
                 else:
                     print(".", end=' ')
-            print('| ' + str(y+1))
+            print('| ' + str(y + 1))
 
         print("    ---------------")
         print("    A B C D E F G H\n")
 
         print("STATISTICS (score / remaining time):")
-        print("Black: {} / {} min {} sec".format(str(self.count(-1)), str(divmod(int(time[-1]),60)[0]), str(divmod(int(time[-1]),60)[1])))
-        print("White: {} / {} min {} sec\n".format(str(self.count(1)), str(divmod(int(time[1]),60)[0]), str(divmod(int(time[1]),60)[1])))
+        print("Black: {} / {} min {} sec".format(str(self.count(-1)), str(divmod(int(time[-1]), 60)[0]),
+                                                 str(divmod(int(time[-1]), 60)[1])))
+        print("White: {} / {} min {} sec\n".format(str(self.count(1)), str(divmod(int(time[1]), 60)[0]),
+                                                   str(divmod(int(time[1]), 60)[1])))
 
     def count(self, color):
         """ Count the number of pieces of the given color.
@@ -68,25 +71,25 @@ class Board():
         count = 0
         for y in range(8):
             for x in range(8):
-                if self[x][y]==color:
+                if self[x][y] == color:
                     count += 1
         return count
 
     def get_squares(self, color):
         """ Get the coordinates (x,y) for all pieces on the board of the given color.
         (1 for white, -1 for black, 0 for empty spaces) """
-        squares=[]
+        squares = []
         for y in range(8):
             for x in range(8):
-                if self[x][y]==color:
-                    squares.append((x,y))
+                if self[x][y] == color:
+                    squares.append((x, y))
         return squares
 
     def get_legal_moves(self, color):
         """ Return all the legal moves for the given color.
         (1 for white, -1 for black) """
         # Store the legal moves
-        if color!=1 and color!=-1:
+        if color != 1 and color != -1:
             return []
         moves = set()
         # Get all the squares with pieces of the given color.
@@ -103,12 +106,12 @@ class Board():
         piece, and (3,5) and (3,6) contain white pieces, and (3,7) is empty,
         one of the returned moves is (3,7) because everything from there to
         (3,4) can be flipped. """
-        (x,y) = square
+        (x, y) = square
         # Determine the color of the piece
         color = self[x][y]
 
         # Skip empty source squares
-        if color==0:
+        if color == 0:
             return None
 
         # Search all possible directions
@@ -128,25 +131,25 @@ class Board():
 
         # Add the piece to the empty square
         flips = (flip for direction in self.__directions
-                      for flip in self._get_flips(move, direction, color))
+                 for flip in self._get_flips(move, direction, color))
 
-        for x,y in flips:
+        for x, y in flips:
             self[x][y] = color
 
     def _discover_move(self, origin, direction):
         """ Return the endpoint of a legal move, starting at the given origin,
         and moving in the given direction. """
-        x,y = origin
+        x, y = origin
         color = self[x][y]
         flips = []
 
-        for x,y in Board._increment_move(origin, direction):
+        for x, y in Board._increment_move(origin, direction):
             if self[x][y] == 0 and flips:
-                return (x,y)
-            elif (self[x][y] == color or (self[x][y] == 0 and not flips)):
+                return x, y
+            elif self[x][y] == color or (self[x][y] == 0 and not flips):
                 return None
             elif self[x][y] == -color:
-                flips.append((x,y))
+                flips.append((x, y))
 
     def _get_flips(self, origin, direction, color):
         """ Get the list of flips for a vertex and a direction to use within
@@ -157,7 +160,7 @@ class Board():
         for x, y in Board._increment_move(origin, direction):
             if self[x][y] == -color:
                 flips.append((x, y))
-            elif (self[x][y] == 0 or (self[x][y] == color and len(flips) == 1)):
+            elif self[x][y] == 0 or (self[x][y] == color and len(flips) == 1):
                 break
             elif self[x][y] == color and len(flips) > 1:
                 return flips
@@ -171,33 +174,38 @@ class Board():
             yield move
             move = list(map(sum, list(zip(move, direction))))
 
+
 def get_col_char(col):
     """ Convert 1, 2, etc. to 'a', 'b', etc. """
-    return chr(ord('a')+col)
+    return chr(ord('a') + col)
+
 
 def move_string(move):
     """ Convert a numeric (x,y) coordinate like (2,3) into a piece name like 'c4'. """
-    (x,y) = move
-    return get_col_char(x)+str(y+1)
+    (x, y) = move
+    return get_col_char(x) + str(y + 1)
+
 
 def moves_string(moves):
     """ Return the given list of coordinates as a nicely formatted list of
     moves. Example: [(2,3),(5,2)] -> 'c4, f3' """
     s = ""
     for i, move in enumerate(moves):
-        if i == len(moves)-1:
+        if i == len(moves) - 1:
             s += move_string(move)
         else:
             s += move_string(move) + ', '
     return s
 
+
 def print_moves(moves):
     """ Print the list of coordinates. """
     print(moves_string(moves))
 
+
 if __name__ == '__main__':
     board = Board()
-    board.display({-1:300,1:250}) # time in seconds
+    board.display({-1: 300, 1: 250})  # time in seconds
 
     print("Black legal moves: ", end='')
     print_moves(board.get_legal_moves(-1))
